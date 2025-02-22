28f2be37b8fc54983b2e37a36786b48b4160398b
arki <antoniorafaelariasromero@gmail.com>
2025-02-21 23:46:23 -0500

    feat(client): remove default styles, add API fetching and routing
    - Removed `App.css` to clean up default styles.
    - Updated `App.jsx` to fetch posts from an external API using `fetch` and `useEffect`. Added error and loading states for better UX.
    - Configured React Router in `main.jsx` to support future routing capabilities.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,50 +1,54 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import { invoke } from "@tauri-apps/api/core";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
+  const [data, setData] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true)
 
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-    setGreetMsg(await invoke("greet", { name }));
-  }
+  useEffect(() => {
+    const getPosts = async () =>  {
+      const uri = import.meta.env.VITE_API_URL
+      try {
+        const res = await fetch(`${uri}posts`);
+        if (!res.ok)  {
+          throw new Error(`response status: ${res.status}`);
+        }
+        const result = await res.json();
+        console.log(result);
+        setData(result)
+      } catch (error) {
+        console.error(error.message);
+        setError(error)
+      } finally {
+        setLoading(false)
+      }
+    };
+    getPosts()
+  }, [])
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error}</p>;
 
   return (
-    <main className="container">
-      <h1>Welcome to Tauri + React</h1>
-
-      <div className="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <>
+      <div>
+        ---
       </div>
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
-
-      <form
-        className="row"
-        onSubmit={(e) => {
-          e.preventDefault();
-          greet();
-        }}
-      >
-        <input
-          id="greet-input"
-          onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
-        />
-        <button type="submit">Greet</button>
-      </form>
-      <p>{greetMsg}</p>
-    </main>
+      {
+        data.map((e, index) => {
+          return (
+            <div key={index}>
+              <p>{e.title}</p>
+              <p>{e.body}</p>
+            </div>
+          )
+        })
+      }
+      <div>
+        ---
+      </div>
+    </>
   );
 }
 
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from 'react-router'
 import App from "./App";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+const root = document.getElementById("root")
+
+ReactDOM.createRoot(root).render(
+  <BrowserRouter>
+    <Routes>
+      <Route  path="/" element={<App />} />
+    </Routes>
+  </BrowserRouter>,
 );
