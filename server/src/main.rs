68a4ef74fc69cb88540d6ec1178d59141e0739c9
arki <antoniorafaelariasromero@gmail.com>
2025-02-21 23:39:54 -0500

    feat(server): add CORS support with tower_http
    - Added CORS support to the server using `tower_http::cors::CorsLayer` with `Any` origin allowed.
    - Integrated `CorsLayer` into the application using `ServiceBuilder` to ensure cross-origin requests are handled properly.

--- server/src/main.rs
+++ server/src/main.rs
@@ -11,13 +11,20 @@ use self::models::*;
 use diesel::prelude::*;
 use server::*;
 use handlers::*;
+use tower_http::cors::{Any, CorsLayer};
+use tower::ServiceBuilder;
 
 #[tokio::main]
 async fn main() {
+
+    let cors_layer = CorsLayer::new()
+        .allow_origin(Any);
+
     let app = Router::new()
         .route("/", get("GAAA"))
         .route("/posts", get(show_posts))
-        .route("/posts/{id}", get(get_post));
+        .route("/posts/{id}", get(get_post))
+        .layer(ServiceBuilder::new().layer(cors_layer));
     let listener = tokio::net::TcpListener::bind("0.0.0.0:8080")
         .await
         .unwrap();
